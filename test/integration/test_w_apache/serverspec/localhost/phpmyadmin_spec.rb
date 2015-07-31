b63d70c1149fbfbae1523ff2c70d3f22a0875992
Full Of Lilies <fulloflilies@gmail.com>
2015-07-31 13:43:05 -0400

    Change phpmyadmin and spec

--- test/integration/test_w_apache/serverspec/localhost/phpmyadmin_spec.rb
+++ test/integration/test_w_apache/serverspec/localhost/phpmyadmin_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe 'w_apache::phpmyadmin' do
 
-	describe file('/opt/phpmyadmin/config.inc.php') do
+	describe file('/websites/phpmyadmin/config.inc.php') do
 	  it { should be_file }
 	end
 
+  describe file('/etc/apache2/conf-available/phpmyadmin.conf') do
+	  it { should be_file }
+	end
+	
 end
