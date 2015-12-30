80b05ec332dd4fcabff721e174551b90b8e24826
Joel Handwell <joelhandwell@gmail.com>
2015-12-31 00:37:19 +0900

    Refactor varnish_integration recipe for warning and readability

--- recipes/varnish_integration.rb
+++ recipes/varnish_integration.rb
@@ -15,23 +15,19 @@ end
 
 node['w_common']['web_apps'].each do |web_app|
 
-  if web_app.has_key? 'varnish' then
+  next unless web_app.has_key?('varnish')
+  next unless web_app['varnish'].has_key?('purge_target')
+  next unless web_app['varnish']['purge_target']
 
-    if web_app['varnish'].has_key? 'purge_target' then
+  Chef::Log.info "Generating hosts file entries for varnish purge target domains for #{web_app['connection_domain']['varnish_domain']}"
 
-      if web_app['varnish']['purge_target'] == true then
-
-        Chef::Log.info "Generating hosts file entries for varnish purge target domains for #{web_app['connection_domain']['varnish_domain']}"
-
-        node['w_varnish']['node_ipaddress_list'].each_with_index do |varnish_node_ip, index|
-          domain = index.to_s + web_app['connection_domain']['varnish_domain']  
-          hostsfile_entry varnish_node_ip do
-            hostname domain
-            action :append
-            unique true
-          end
-        end
-      end
+  node['w_varnish']['node_ipaddress_list'].each_with_index do |varnish_node_ip, index|
+    domain = "#{index}#{web_app['connection_domain']['varnish_domain']}"
+    hostsfile_entry "#{varnish_node_ip} #{domain} for #{web_app['vhost']['main_domain']}" do
+      ip_address varnish_node_ip
+      hostname domain
+      action :append
+      unique true
     end
   end
 end
