fb47d943eb4d56c8ab0d5503883f5ec498b71660
Joel Handwell <joelhandwell@gmail.com>
2015-12-30 22:30:52 +0900

    Proper chefspec test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,21 +3,20 @@ require 'chefspec/berkshelf'
 require 'mymatchers'
 
 ChefSpec::Coverage.start! do
-  add_filter(%r{/apt/})
-  add_filter(%r{/monit/})
-  add_filter(%r{/apache2/})
-  add_filter(%r{/php/})
-  add_filter(%r{/phpmyadmin/})
-  add_filter(%r{/xdebug/})
-  add_filter(%r{/build-essential/})
-  add_filter(%r{/git/})
-  add_filter(%r{/apt-repo/})
-  add_filter(%r{/nfs/})
-  add_filter(%r{/haproxy/})  
+  add_filter(%r{apt})
+  add_filter(%r{apache2})
+  add_filter(%r{php})
+  add_filter(%r{phpmyadmin})
+  add_filter(%r{xdebug})
+  add_filter(%r{monit})
+  add_filter(%r{build-essential})
+  add_filter(%r{git})
+  add_filter(%r{apt-repo})
+  add_filter(%r{nfs})
+  add_filter(%r{haproxy})
 end
 
 RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version = '14.04'
-  config.filter_run_excluding skip: true
 end
