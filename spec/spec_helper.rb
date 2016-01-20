7cc3531c3102ad79bf7b79cffc9a1877658bfd31
Joel Handwell <joelhandwell@gmail.com>
2016-01-20 16:17:02 -0500

    Chefspec outputs proper converge report, update travis config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,17 +3,17 @@ require 'chefspec/berkshelf'
 require 'mymatchers'
 
 ChefSpec::Coverage.start! do
-  add_filter(%r{apt})
-  add_filter(%r{apache2})
-  add_filter(%r{php})
-  add_filter(%r{phpmyadmin})
-  add_filter(%r{xdebug})
-  add_filter(%r{monit})
-  add_filter(%r{build-essential})
-  add_filter(%r{git})
-  add_filter(%r{apt-repo})
-  add_filter(%r{nfs})
-  add_filter(%r{haproxy})
+  add_filter(%r{[\/\\]apt[\/\\]})
+  add_filter(%r{[\/\\]apache2[\/\\]})
+  add_filter(%r{[\/\\]php[\/\\]})
+  add_filter(%r{[\/\\]phpmyadmin[\/\\]})
+  add_filter(%r{[\/\\]xdebug[\/\\]})
+  add_filter(%r{[\/\\]monit[\/\\]})
+  add_filter(%r{[\/\\]build-essential[\/\\]})
+  add_filter(%r{[\/\\]git[\/\\]})
+  add_filter(%r{[\/\\]apt-repo[\/\\]})
+  add_filter(%r{[\/\\]nfs[\/\\]})
+  add_filter(%r{[\/\\]haproxy[\/\\]})
 end
 
 RSpec.configure do |config|
