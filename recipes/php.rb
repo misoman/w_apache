75a0d62ef6979cf1ea21578c2aaf837549ea133f
Joel Handwell <joelhandwell@gmail.com>
2016-02-19 21:01:33 -0800

    Adjust socket name with official chef-php default

--- recipes/php.rb
+++ recipes/php.rb
@@ -7,7 +7,7 @@ php_fpm 'php-fpm' do
   user 'www-data'
   group 'www-data'
   socket true
-  socket_path '/var/run/php-fpm-www.sock'
+  socket_path '/var/run/php5-fpm.sock'
   terminate_timeout (node['php']['ini_settings']['max_execution_time'].to_i + 20)
   valid_extensions %w( .php .htm .php3 .html .inc .tpl .cfg )
   value_overrides({
