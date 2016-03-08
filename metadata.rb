87b849597ab71fe77de032325a037f435138f947
Joel Handwell <joelhandwell@gmail.com>
2016-03-08 09:57:34 -0500

    Satisfy foodcritic check

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'apachev2'
 description      'Installs/Configures apache2, php, php fpm and other related packages used in web server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.4'
+issues_url       'https://github.com/haapp/w_apache/issues'
+source_url       'https://github.com/haapp/w_apache'
 
 depends 'apt'
 depends 'ubuntu'
