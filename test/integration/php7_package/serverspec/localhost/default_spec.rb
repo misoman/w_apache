5b5f813347e855483523218342d9baa539f87c5b
Joel Handwell <joelhandwell@gmail.com>
2016-06-06 11:44:14 -0400

    Simplify test names

--- test/integration/php7_package/serverspec/localhost/default_spec.rb
+++ test/integration/php7_package/serverspec/localhost/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 %w( blackfire composer default deploy monit newrelic_app php phpmyadmin ssl varnish_integration vhosts ).each do |test|
   require_relative "../w_apache/#{test}_spec.rb"
 
-  describe "php7 package #{test} installation and configration" do
+  describe test do
     include_examples "w_apache::#{test}"
   end
 end
@@ -11,7 +11,7 @@ end
 %w( mysql mysqli session ).each do |test|
   require_relative "../w_apache/php_ini/#{test}_spec.rb"
 
-  describe "php ini #{test} installation and configration" do
+  describe "php ini #{test}" do
     include_examples "w_apache::php_ini::#{test}"
   end
 end
