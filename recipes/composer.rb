1579b5185e1230414b1a7e5719c3ece5028caac8
Miso Podolak <miso.podolak@gmail.com>
2016-10-03 14:07:46 +0200

    add existencional condition for composer cache directory

--- recipes/composer.rb
+++ recipes/composer.rb
@@ -1,8 +1,9 @@
+include_recipe 'composer'
+
 directory "make sure ownership and existance of composer home dir #{node['w_apache']['home']}/composer" do
 	path "#{default['w_apache']['home']}/composer"
 	owner node['w_apache']['owner']
 	group node['w_apache']['group']
 	recursive true
+	not_if do ::Dir.exists?("#{default['w_apache']['home']}/composer") end
 end
-
-include_recipe 'composer'
